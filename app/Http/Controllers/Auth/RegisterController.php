9904f753044d0d779cccc4d93856ae1aa4aba39a
charleeo <charleeotaru@gmail.com>
2021-09-22 23:38:39 +0100

    authenetication

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,136 +2,72 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Exceptions\Helpers\Helper;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\RegisterUserRequest;
+use App\Providers\RouteServiceProvider;
 use App\Models\User;
-use App\Services\AppDateUtils;
-use App\Services\AppUtils;
-use App\Services\LogUtils;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\ValidationException;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use PhpParser\Node\Stmt\TryCatch;
-use Throwable;
 
 class RegisterController extends Controller
 {
-    // use AuthenticatesUsers;
-    protected $maxAttempts;
-    protected $decayMinutes;
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
     public function __construct()
     {
-        $this->maxAttempts = config('const.authMaxAttempt');
-        $this->decayMinutes = config('const.authThrottleTimeMin');
+        $this->middleware('guest');
     }
 
-    public function registerUser(RegisterUserRequest $request){
-        $status=false;
-        $responseData = null;
-        $error =null;
-        $responseMessage='';
-       $status = false;
-       $validatedData = $request->validated();
-       
-       $validatedData['password'] = bcrypt($request->password);
-       try{
-           $user = User::create($validatedData) ;      
-           $responseMessage = "record created successfully";
-           $accessToken = $user->createToken(config('const.token'))->accessToken;
-           $status =true;
-           $responseData = $accessToken;
-           
-        }catch(Throwable $ex){
-            $responseMessage = "There was an error";
-            $error = LogUtils::errorLog($ex);
-        }
-        
-        $res = AppUtils::formatJson($responseMessage,$status,$responseData);
-       Helper::write_log(LogUtils::getLogData($request, $error ? $error : $res, 'RegisterContrller@register'));
-        return $res;
-    }
-
-    public function login(Request $request){
-        $status=false;
-        $error = false;
-        $responseData=null;
-        $responseMessage = '';
-
-        $data =$request->validate([
-         'email'=>['required','string','email'],
-         'password'=>['required','string'],
-         'remember_me'=>['nullable','boolean']
-        ]);
-
-        try{
-            
-            if(Auth::attempt($data)){
-                // $user  = User::find(auth()->id());
-                $user = $request->user();
-                
-                $tokenResult = $user->createToken(config('const.token'));
-
-                if ($request->remember_me) {
-                    $tokenResult->expires_at = Carbon::now()->addWeeks(1);
-                }
-
-                $responseData = [
-                    'access_token' => $tokenResult->accessToken,
-                    'token_type' => 'Bearer',
-                    'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
-                ];
-                $status =true;
-            }else{
-                $responseMessage= 'Invalid credentials';
-            }
-
-        }catch(Throwable $ex){
-            $responseMessage = 'Error has occured.';
-            $error = LogUtils::errorLog($ex);
-        }
-
-        $res= AppUtils::formatJson($responseMessage,$status,$responseData);
-         Helper::write_log(LogUtils::getLogData($request, $error ? $error : $res,'RegisterController@register'));
-         return $res;        
-    }
-
-    
-    public function logout(Request $request){
-        $responseMessage ='';
-        $error = null;
-        $status=false;
-        try{
-            $request->user()->token()->revoke();
-            $responseMessage = "Logout successfully";
-            $status=true;
-        }catch(Throwable $ex){
-            $error = LogUtils::errorLog($ex);
-            $responseMessage = "There was an error loging you out";
-        }
-      $res = AppUtils::formatJson($responseMessage,$status);
-      Helper::write_log(LogUtils::getLogData($request,$error?$error:$res,'Logging out'));
-      return $res;
-    }
-
-    public function authUser(Request $request){
-        $user = $request->user();
-        return $user;
-    }
-    protected function sendLockoutResponse(Request $request)
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
     {
-        $seconds = $this->limiter()->availableIn(
-            $this->throttleKey($request)
-        );
-    
-        throw ValidationException::withMessages([
-            $this->username() => 'Maximum auth attempts exceeded. Please try again after ' . AppDateUtils::secondsToTime($seconds),
-        ])->status(Response::HTTP_TOO_MANY_REQUESTS);
-    
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\Models\User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
     }
 }
